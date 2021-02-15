39882ca230678dac361b76959cae8eee7a000f76
Fernando Paravela <fernandoparavela@gmail.com>
2021-02-15 18:27:42 -0300

    title fix

--- pages/onze22.js
+++ pages/onze22.js
@@ -42,7 +42,7 @@ export default function Home() {
   <div>
 
       <Head>
-        <title>Harmonia 1040 · Idea!Zarvos</title>
+        <title>Onze 22 · Idea!Zarvos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
