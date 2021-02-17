ffbcf466f8bb78b5b662649bb5d275e07cbcd9e9
Fernando Paravela <fernandoparavela@gmail.com>
2021-02-17 11:07:27 -0300

    viewport fix

--- pages/popgrafite2.js
+++ pages/popgrafite2.js
@@ -44,7 +44,7 @@ export default function Home() {
       <Head>
         <title>Pop Grafite · Idea!Zarvos</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="initial-scale=1.0, user-scalable=no"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
       </Head>
 
       <Header/>
