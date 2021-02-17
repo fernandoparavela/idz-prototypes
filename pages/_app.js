59e9b45efcadb0eaf8ed6575846c120ce20f9c54
Fernando Paravela <fernandoparavela@gmail.com>
2021-02-17 11:51:16 -0300

    overflow

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <div className="wrap">
+      <div>
           <Component {...pageProps} />
       </div>
     )
